c145ab94ab5ccf625ae88beec98f4de4e7e7e714
Michael (Enzian) Ingold <github@michaelingold.ch>
2016-08-04 14:38:15 +0200

    added timing metric implementation

--- src/System.Metrics.Asp.Mvc/MetricHandlers.cs
+++ src/System.Metrics.Asp.Mvc/MetricHandlers.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
 
 namespace System.Metrics.Asp.Mvc
 {
@@ -10,6 +11,24 @@ namespace System.Metrics.Asp.Mvc
             {
                 endpoint.Record<Counting>(metric, increment);
                 return next();
+            };
+
+            return handler;
+        }
+
+        
+        public static MetricsHandler Timer(string metric)
+        {
+            MetricsHandler handler = async (Endpoint endpoint, ResourceExecutingContext context, ResourceExecutionDelegate next) =>
+            {
+                var stopwatch = Stopwatch.StartNew();
+                
+                var t = await next();
+
+                stopwatch.Stop();
+                endpoint.Record<Timing>(metric, (int)stopwatch.ElapsedMilliseconds);
+
+                return t;
             };
 
             return handler;
